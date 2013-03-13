8f75c2834a03b9a9217039b2954d785b8986a325
Tom Janssens <tom@timble.net>
2013-03-13 19:41:08 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Nooku</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	
+	<!-- Le styles -->
+	<link href="css/default.css?v=2" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">	
+</head>
+
+<body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="#">Nooku</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+                <li class="active"><a href="#">About</a></li>
+                <li><a href="#about">Framework</a></li>
+                <li><a href="#server">Server</a></li>
+                <li><a href="#blog">Community</a></li>
+                <li><a href="#blog">Blog</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+    
+    <div class="container">
+        <div class="span12">
+            <h1>A platform for creating web applications with less code.</h1>
+            <p class="lead">By drastically reducing the amount of code you need to write, Nooku helps you to create powerful web applications easily. Using the web technologies you know and love: PHP, HTML, CSS, and JavaScript.</p>
+            <p class="lead">Nooku is a free and open source toolkit that allows you to create applications quickly. The intuitive API, design pattern based architecture, and out-of-the-box security, leave you more time to focus on features.</p>
+        </div>
+    </div>
+    
+    <div class="container">
+        <div class="span4">
+            <h3>Read our Blog</h3>        
+            <ul>
+                <li><a href="http://feedproxy.google.com/~r/nooku/blog/~3/WiTUWLjtM7k/">Announcing Nooku Jam 12.3 Leuven!</a></li>
+                <li><a href="http://feedproxy.google.com/~r/nooku/blog/~3/WN8tfEU_q6g/">Looking back at Nooku Jam 12.2</a></li>
+                <li><a href="http://feedproxy.google.com/~r/nooku/blog/~3/x1BGF_R4WWI/">Activity Streams with com_activities</a></li>
+                <li><a href="http://feedproxy.google.com/~r/nooku/blog/~3/jzjeAa2WyPk/">Sunsetting Nooku Content</a></li>
+                <li><a href="http://feedproxy.google.com/~r/nooku/blog/~3/IXmTmi0lSmA/">Nooku is 4!</a></li>
+            </ul>
+        </div>
+        <div class="span4">
+            <h3>Follow Us on Twitter</h3>
+            <p>
+                <a href="http://search.twitter.com/search?q=%23Anahita" title="#Anahita">#Anahita</a> accepted into @<a href="https://twitter.com/MozWebFWD">MozWebFWD</a> <a href="http://t.co/betmN6dKfX">http://t.co/betmN6dKfX</a> Congrats @<a href="https://twitter.com/rastin">rastin</a> @<a href="https://twitter.com/peerglobe">peerglobe</a>
+            </p>
+        </div>
+        <div class="span4">
+            <h3>Join Us on Facebook</h3>
+            <p class="center" style="margin-top:10px;"><a href="http://www.facebook.com/nooku.org" target="_blank"><img border="0" src="http://www.nooku.org/images/stories/icons/facebook-like.png" alt="Like us on Facebook"></a><br> <a href="http://www.facebook.com/nooku.org" target="_blank">facebook.com/nooku.org</a></p>
+            <p class="center">Feel free to swing by and say hi!</p>
+        </div>
+    </div>
+
+    <script src="components/jquery/jquery.min.js"></script>
+    <script src="components/bootstrap/js/bootstrap-collapse.js"></script>
+</body>
+</html>
