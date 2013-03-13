07b2e508521e511b131c7ea401abb839235c0eaa
Tom Janssens <tom@timble.net>
2013-03-13 19:54:25 +0100

    Initial work

--- index.php
+++ index.php
@@ -36,34 +36,38 @@
     </div>
     
     <div class="container">
-        <div class="span12">
-            <h1>A platform for creating web applications with less code.</h1>
-            <p class="lead">By drastically reducing the amount of code you need to write, Nooku helps you to create powerful web applications easily. Using the web technologies you know and love: PHP, HTML, CSS, and JavaScript.</p>
-            <p class="lead">Nooku is a free and open source toolkit that allows you to create applications quickly. The intuitive API, design pattern based architecture, and out-of-the-box security, leave you more time to focus on features.</p>
+        <div class="row">
+            <div class="span12">
+                <h1>A platform for creating web applications with less code.</h1>
+                <p class="lead">By drastically reducing the amount of code you need to write, Nooku helps you to create powerful web applications easily. Using the web technologies you know and love: <strong>PHP</strong>, <strong>HTML</strong>, <strong>CSS</strong>, and <strong>JavaScript</strong></p>
+                <p class="lead">The intuitive API, design pattern based architecture, and out-of-the-box security, leave you more time to focus on features.</p>
+            </div>
         </div>
     </div>
     
     <div class="container">
-        <div class="span4">
-            <h3>Read our Blog</h3>        
-            <ul>
-                <li><a href="http://feedproxy.google.com/~r/nooku/blog/~3/WiTUWLjtM7k/">Announcing Nooku Jam 12.3 Leuven!</a></li>
-                <li><a href="http://feedproxy.google.com/~r/nooku/blog/~3/WN8tfEU_q6g/">Looking back at Nooku Jam 12.2</a></li>
-                <li><a href="http://feedproxy.google.com/~r/nooku/blog/~3/x1BGF_R4WWI/">Activity Streams with com_activities</a></li>
-                <li><a href="http://feedproxy.google.com/~r/nooku/blog/~3/jzjeAa2WyPk/">Sunsetting Nooku Content</a></li>
-                <li><a href="http://feedproxy.google.com/~r/nooku/blog/~3/IXmTmi0lSmA/">Nooku is 4!</a></li>
-            </ul>
-        </div>
-        <div class="span4">
-            <h3>Follow Us on Twitter</h3>
-            <p>
-                <a href="http://search.twitter.com/search?q=%23Anahita" title="#Anahita">#Anahita</a> accepted into @<a href="https://twitter.com/MozWebFWD">MozWebFWD</a> <a href="http://t.co/betmN6dKfX">http://t.co/betmN6dKfX</a> Congrats @<a href="https://twitter.com/rastin">rastin</a> @<a href="https://twitter.com/peerglobe">peerglobe</a>
-            </p>
-        </div>
-        <div class="span4">
-            <h3>Join Us on Facebook</h3>
-            <p class="center" style="margin-top:10px;"><a href="http://www.facebook.com/nooku.org" target="_blank"><img border="0" src="http://www.nooku.org/images/stories/icons/facebook-like.png" alt="Like us on Facebook"></a><br> <a href="http://www.facebook.com/nooku.org" target="_blank">facebook.com/nooku.org</a></p>
-            <p class="center">Feel free to swing by and say hi!</p>
+        <div class="row">
+            <div class="span4">
+                <h3>Read our Blog</h3>        
+                <ul>
+                    <li><a href="http://feedproxy.google.com/~r/nooku/blog/~3/WiTUWLjtM7k/">Announcing Nooku Jam 12.3 Leuven!</a></li>
+                    <li><a href="http://feedproxy.google.com/~r/nooku/blog/~3/WN8tfEU_q6g/">Looking back at Nooku Jam 12.2</a></li>
+                    <li><a href="http://feedproxy.google.com/~r/nooku/blog/~3/x1BGF_R4WWI/">Activity Streams with com_activities</a></li>
+                    <li><a href="http://feedproxy.google.com/~r/nooku/blog/~3/jzjeAa2WyPk/">Sunsetting Nooku Content</a></li>
+                    <li><a href="http://feedproxy.google.com/~r/nooku/blog/~3/IXmTmi0lSmA/">Nooku is 4!</a></li>
+                </ul>
+            </div>
+            <div class="span4">
+                <h3>Follow Us on Twitter</h3>
+                <p>
+                    <a href="http://search.twitter.com/search?q=%23Anahita" title="#Anahita">#Anahita</a> accepted into @<a href="https://twitter.com/MozWebFWD">MozWebFWD</a> <a href="http://t.co/betmN6dKfX">http://t.co/betmN6dKfX</a> Congrats @<a href="https://twitter.com/rastin">rastin</a> @<a href="https://twitter.com/peerglobe">peerglobe</a>
+                </p>
+            </div>
+            <div class="span4">
+                <h3>Join Us on Facebook</h3>
+                <p class="center" style="margin-top:10px;"><a href="http://www.facebook.com/nooku.org" target="_blank"><img border="0" src="http://www.nooku.org/images/stories/icons/facebook-like.png" alt="Like us on Facebook"></a><br> <a href="http://www.facebook.com/nooku.org" target="_blank">facebook.com/nooku.org</a></p>
+                <p class="center">Feel free to swing by and say hi!</p>
+            </div>
         </div>
     </div>
 
