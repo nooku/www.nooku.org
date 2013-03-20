7f0d040564c48b0bc70c23fc5e705b07a8594938
Tom Janssens <tom@timble.net>
2013-03-20 18:24:48 +0100

    Major changes

---
+++ js/application.js
@@ -0,0 +1,13 @@
+// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
+// IT'S ALL JUST JUNK FOR OUR DOCS!
+// ++++++++++++++++++++++++++++++++++++++++++
+
+!function ($) {
+
+  $(function(){
+
+    // make code pretty
+    window.prettyPrint && prettyPrint()
+  })
+
+}(window.jQuery)
